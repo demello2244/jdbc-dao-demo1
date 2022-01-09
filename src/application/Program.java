7b567ce7afb8935e0833e7397c59881b8f1be943
Denise Mello <denisemello.zero@gmail.com>
2022-01-09 18:03:55 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department dp = new Department(1, "Comercial");
-		System.out.println(dp);
-		
+		Department dp = new Department(1, "Comercial");		
+		Seller seller = new Seller(1, "Denise", "denise@gmail.com", new Date(), 2000.0, dp);
+		System.out.println(seller);
 	}
 
 }
